68c438b4166ccab461b5520dd3549e15c6470e31
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-05-15 21:06:07 +0200

    style

--- script/code.js
+++ script/code.js
@@ -2,3 +2,8 @@ let dataEntry = document.querySelector('[data-entry]')
 function display(value) {
     dataEntry.value += value
 }
+
+// let dataOperator = document.querySelector('[data-operator]')
+// function display(value) {
+//     dataOperator.value += value
+// }
